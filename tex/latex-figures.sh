0bb737401c905b7c6477c85fe8a0312d474b2624
david.villa <unknown>
2012-03-01 19:55:29 +0000

    latex-figures ignore included graphics starting with "/"

--- tex/latex-figures.sh
+++ tex/latex-figures.sh
@@ -3,4 +3,5 @@ MAIN=${1:-main.tex}
 [ -f $MAIN ] && grep -v "^ *%" $MAIN \
     | grep includegraphics \
     | sed "s/.*includegraphics.*{\([.a-zA-Z0-9_//\-]*\).*$/\1/g" \
+    | grep -v ^/ \
     | sort | uniq
