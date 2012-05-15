0bc569d529d4e75de9f303aa8ebc38cdd7f417c1
David Villa Alises <David.Villa@gmail.com>
2012-05-15 15:45:20 +0200

    script shorcuts for makefiles

--- make/pdfjam.mk
+++ make/pdfjam.mk
@@ -9,7 +9,7 @@ PDFNUP=pdfnup --quiet --paper a4paper
 DELTA08=--delta  "0.8cm 0.8cm"
 
 %.2x1.pdf: %.pdf
-	$(PDFNUP) $(DELTA08) --frame true --nup 2x1 $< --outfile $@
+	$(PDFNUP) $(DELTA08) --landscape --nup 2x1 $< --outfile $@
 
 %.1x3.pdf: %.pdf
 	$(PDFNUP) $(DELTA08) --no-landscape --scale 0.9 --frame true --offset "-3cm 0" --nup 1x3 $< --outfile $@
