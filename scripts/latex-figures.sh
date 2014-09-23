e96dd4c2b38c8b2c47d716b385cc9a96e80fc65f
Oscar Aceña <oscaracena@gmail.com>
2014-09-23 09:59:48 +0200

    support on latex-figures.sh to download images from URLs

--- scripts/latex-figures.sh
+++ scripts/latex-figures.sh
@@ -1,7 +1,34 @@
 #!/bin/bash
+
 MAIN=${1:-main.tex}
-[ -f $MAIN ] && grep -v "^ *%" $MAIN \
-    | grep includegraphics \
-    | sort | uniq \
-    | sed "s/.*includegraphics.*{\([.a-zA-Z0-9_//\-]*\).*$/\1/g" \
-    | grep -v ^/
+VALID_CHARS=":!#$&@%+/-.a-Z0-9_//\-"
+
+get_figures() {
+    [ -f $MAIN ] && grep -v "^ *%" $MAIN \
+	| grep includegraphics \
+	| sed "s/.*includegraphics.*{\([$VALID_CHARS]*\).*$/\1/g" \
+	| sort | uniq \
+	| grep -v ^/
+}
+
+print() {
+    for i in $FIGURES; do
+	if [[ $(echo $i | grep -o "http://.*") ]]; then
+	    basename $i;
+	else
+	    echo $i
+	fi
+    done
+}
+
+download() {
+    for i in $FIGURES; do
+	if [[ $(echo $i | grep -o "http://.*") ]]; then
+	    wget $i -O $(basename $i) -q
+	fi
+    done
+}
+
+FIGURES=$(get_figures)
+download
+print
